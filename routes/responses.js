2458bc6a8be769cf7c85923765b7a2dbf18de552
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2023-02-27 16:51:28 +0530

    response routes tested

--- routes/responses.js
+++ routes/responses.js
@@ -14,15 +14,15 @@ const router = express.Router()
 router.get('/all',getResponses)
 
 //get response by uId and rId
-router.get('/uId/rId',getResponse)
+router.get('/:uId/:rId',getResponse)
 
 //create response
 router.post('/add',createResponse)
 
 //update response by uId and rId
-router.patch('/update/uId/rId',updateResponse)
+router.patch('/update/:uId/:rId',updateResponse)
 
 //delete response by uId and rId
-router.delete('/delete/uId/rId',deleteResponse)
+router.delete('/delete/:uId/:rId',deleteResponse)
 
 module.exports=router;
